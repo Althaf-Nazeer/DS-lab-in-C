d30f3c590397c2981fd01191d23f2d4937f7335e
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-28 23:50:59 +0530

    Create Insertionsort.c

---
+++ Insertionsort.c
@@ -0,0 +1,13 @@
+// Function to perform insertion sort
+void insertionSort(int arr[], int size) {
+    int i, key, j;
+    for (i = 1; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
