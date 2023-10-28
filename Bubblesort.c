2df763eba944a59826dd59f536bebb9104d6c3c1
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-28 23:50:04 +0530

    Create Bubblesort.c

---
+++ Bubblesort.c
@@ -0,0 +1,13 @@
+// Function to perform bubble sort
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap if the elements are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
