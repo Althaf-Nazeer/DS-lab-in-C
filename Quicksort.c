0c0c680d338d71825ab15695f9b4776f57b29c08
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-29 00:04:08 +0530

    Create Quicksort.c

---
+++ Quicksort.c
@@ -0,0 +1,29 @@
+// Partition function for quick sort
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+// Quick sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
